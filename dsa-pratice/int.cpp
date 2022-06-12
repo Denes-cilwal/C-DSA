a8a89dea72d93e8bcac89c22537c37b444997b07
Denes-cilwal <dinesh.silwal@wesionary.team>
2022-06-12 19:03:44 +0545

    initial commit

---
+++ dsa-pratice/int.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using  namespace std;
+int main() {
+    int a ; // var  declaration
+    a =  12 ; // initialization
+    cout << "size of int" << sizeof(a) << endl;
+    float  b;
+    cout << "size of float" << sizeof(b) << endl;
+    char c;
+    cout << "size of char" << sizeof(c) << endl;
+    return 0;
+}
+
